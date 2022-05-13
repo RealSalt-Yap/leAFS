4eb670e39c32e773c5f68eedd9fbc874c31f34fc
Yap Zhen Yan <105295035+RealSalt-Yap@users.noreply.github.com>
2022-05-13 14:51:27 +0800

    Update Master_mini.ino

--- Master_mini.ino
+++ Master_mini.ino
@@ -1,18 +1,16 @@
-#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Servo.h>  
 #define liq_trigPin 3                  // ultrasonic sensor for liq
 #define liq_echoPin 4                 // ultrasonic sensor for liq
 #define trigPin 10                      // ultrasonic sensor for manual mode
 #define echoPin 11                    // ultrasonic sensor for manual mode
-#define System_pin 7                  // on and off
-#define Mode_pin 6                    // manual & automatic
+#define System_pin 6                  // on and off
+#define Mode_pin 5                    // manual & automatic
 #define buzzer A2
 #define BlueLED 12
 #define GreenLED 13
-#define RedLED 8
-#define Blue 9
-#define fan 2
+#define RedLED 7
+#define Blue 8 
 int system_state;
 int mode_state;
 bool buzz = true;
@@ -22,7 +20,6 @@ Servo myservo;
 
 void setup() {
 
-//Serial.begin(9600);
 Wire.begin();
 pinMode(System_pin, INPUT);
 pinMode(Mode_pin, INPUT);
@@ -35,8 +32,7 @@ pinMode(liq_trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
 pinMode(liq_echoPin, INPUT); // Sets the echoPin as an INPUT
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
 pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-//pinMode(fan, OUTPUT);
-myservo.attach(5);            
+myservo.attach(9);            
 }
 
 
@@ -65,7 +61,6 @@ void loop() {
     Wire.endTransmission();
     digitalWrite(BlueLED, LOW);
     digitalWrite(GreenLED,HIGH);
-    digitalWrite(fan, HIGH);
     spray();                           // spray mechanism
     delay(500);
     return;
@@ -81,7 +76,6 @@ void loop() {
     Wire.endTransmission();
     digitalWrite(GreenLED, LOW);
     digitalWrite(BlueLED, HIGH);
-    digitalWrite(fan, HIGH);
     manual_detection();                        // manual detection + spray
     return;
   }
@@ -97,7 +91,6 @@ void loop() {
     digitalWrite(BlueLED, LOW);   
     digitalWrite(RedLED, LOW);
     digitalWrite(Blue, LOW); 
-    digitalWrite(fan, LOW);
     return;
   }
 }
@@ -117,13 +110,13 @@ void liq_detection()
   // Distance is half the duration devided by 29.1 (from datasheet)
   liq_distance = (liq_duration/2) / 29.1;
   delay(500);
-  if (liq_distance <= 6)   // suff liq.
+  if (liq_distance >=0 && liq_distance <= 6)   // suff liq.
     {
       liq = 0;
       digitalWrite(Blue, LOW);
       digitalWrite(RedLED, LOW);           
     }
-    else if (liq_distance >= 9)      // no liq
+    else if (liq_distance >= 12)      // no liq
     {
       liq = 2;
       digitalWrite(Blue, LOW);
@@ -141,14 +134,14 @@ void liq_detection()
 
 void manual_spray()
 {
-  myservo.attach(5);
+  myservo.attach(9);
   myservo.write(0);    // turns
-  delay(900);                
+  delay(800);                
   myservo.detach();    // stops 
   delay(2000);         
-  myservo.attach(5);
+  myservo.attach(9);
   myservo.write(170);  // turns
-  delay(650);         
+  delay(250);         
   myservo.detach();    // stops
   delay(2000);         
   return;
@@ -161,11 +154,11 @@ void manual_detection()
   delayMicroseconds(2);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(10);
-
   duration = pulseIn(echoPin, HIGH);    // Measure the pulse input in echo pin
   distance = (duration/2) / 29.1;         // Distance is half the duration devided by 29.1 (from datasheet)
   delay(500);
-  if (distance <= 7)
+  
+  if (distance>= 0 && distance <= 7)
     {
       tone(buzzer, 300, 300);  // buzzer will buzz to show successful detection
       delay(2000);
@@ -188,12 +181,12 @@ void manual_detection()
 
 void spray()
 { 
-  for (int j = 0 ; j <= 6; j++)
+  for (int j = 0 ; j <= 12; j++)
   {
     mode_state = digitalRead(Mode_pin);
     system_state = digitalRead(System_pin);
-    delay(1000);
-    if (j == 6)
+    delay(500);
+    if (j == 12)
     {
       for (int i =0; i<=2; i++)   // buzzer will buzz 3times before spraying
       {
